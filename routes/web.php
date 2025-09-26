af071b732d47d5804aa5e3fe746e84d300f6a773
Habiburrahman2007 <habibmas2007@gmail.com>
2025-09-26 15:35:11 +0700

    pisahin login filament dan livewire

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Filament\Auth\Pages\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->group(function () {
+    Route::get('/login', Login::class)->name('login');
 });
