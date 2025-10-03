741ee7aad3632cab4c3941302b618923ca659def
Habiburrahman2007 <habibmas2007@gmail.com>
2025-10-03 20:12:33 +0700

    nambahin add porto

--- database/migrations/2025_09_22_231000_create_portfolios_table.php
+++ database/migrations/2025_09_22_231000_create_portfolios_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image_url')->nullable();
+            $table->date('created_date');
             $table->timestamps();
+
+            // Optional: foreign key constraints
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\AddPortofolio;
 use App\Livewire\Auth\Register;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Auth\Login;
@@ -16,4 +17,4 @@ Route::get('/dashboard', HomePage::class)
     ->name('dashboard')
     ->middleware('auth');
 
-    
+Route::get('/add-portfolio', AddPortofolio::class);
