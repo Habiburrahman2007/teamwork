c0890a3d2c44b9465f9804c023b5d9428395d5af
Habiburrahman2007 <habibmas2007@gmail.com>
2025-10-04 22:51:42 +0700

    dashboard dinamis

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'App' }}</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
     {{-- Styles Livewire --}}
     @livewireStyles
